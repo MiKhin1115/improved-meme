46b0d1e9d121787d6ccd4d851d3d04a628f054c1
Han Thar <mikhinhantharsoe@uit.edu.mm>
2023-11-07 11:26:42 +0630

    Repair Search on Google

--- Instagram/search.js
+++ Instagram/search.js
@@ -1,73 +1,90 @@
 const myMovies =[
     {
       id: 1,
-      name: "Oppenheimer",
-      url: "Home/bothisweek/oppenheimer.html", // Added 'url' property
+      name: "Ash Ketchum",
+      url: "Home/bothisweek/oppenheimer.html", 
+    },
+    {
+      id: 2,
+      name: "Team Flare",
+      url: "Home/bothisweek/barbie.html", 
+    },
+    { 
+      id: 3,
+      name: "Brock",
+      url: "Home/bothisweek/elemental.html", 
+    },
+    {
+      id: 4,
+      name: "May",
+      url: "Home/bothisweek/hauntedmansion.html", 
+    },
+    {
+      id: 5,
+      name: "Hilbert",
+      url: "Home/bothisweek/indianaJones.html", 
     },
   ];
-  
-  const search3 = document.getElementsByClassName('search3')[0];
-  const search_bar2 = document.getElementById('search_bar2');
-  
-  // Function to show search3
-  function showSearch3() {
-      search3.style.display = "block";
-  }
-  
-  // Function to hide search3
-  function hideSearch3() {
-      search3.style.display = "none";
-  }
-  
-  // Add a click event listener to search_bar2 to show search3
-  search_bar2.addEventListener('click', (event) => {
-      event.stopPropagation(); // Prevent the click event from reaching the document click event
-      showSearch3();
-  });
-  
-  // Add a focus event listener to search_bar2 to show search3
-  search_bar2.addEventListener('focus', () => {
-      showSearch3();
-  });
-  
-  // Add a click event listener to the document to hide search3 when clicking outside
-  document.addEventListener('click', () => {
-      hideSearch3();
-  });
-  
-  window.addEventListener("load", () => {
-    myMovies.forEach((element) => {
-        const { img, name, url } = element;
-  
-        let card = document.createElement("a");
-        card.href = url;
-        card.innerHTML = `
-            <div class="movie1"> 
-            
-            <div class="column">
-            <div class="text">${name}</div>
-            </div>
-            </div>`;
-        search3.appendChild(card);
+    const PopupSearchbox = document.getElementsByClassName('PopupSearchbox')[0];
+    const searchBar = document.getElementById('searchBar');
+    
+    
+    function showPopupSearchbox() {
+        PopupSearchbox.style.display = "block";
+    }
+    
+    function hidePopupSearchbox() {
+        PopupSearchbox.style.display = "none";
+    }
+ 
+    searchBar.addEventListener('click', (event) => {
+        event.stopPropagation(); 
+        showPopupSearchbox();
     });
+
+    searchBar.addEventListener('focus', () => {
+        showPopupSearchbox();
+    });
+
+    document.addEventListener('click', () => {
+        hidePopupSearchbox();
+    });
+
+window.addEventListener("load", () => {
+  myMovies.forEach((element) => {
+      const { name, url } = element;
+
+      let card = document.createElement("a");
+      card.href = url;
+      card.innerHTML = `
+          <div class="column">
+          <div class="text">${name}</div>
+          </div>`;
+          PopupSearchbox.appendChild(card);
   });
-  
-  search_bar2.addEventListener('keyup', () => {
-    let filter = search_bar2.value.toUpperCase();
-    let a = search3.getElementsByTagName('a');
-  
+});
+
+searchBar.addEventListener('keyup', () => {
+    let filter = searchBar.value.toUpperCase();
+    let a = PopupSearchbox.getElementsByTagName('a');
+    let noMatches = true;
+
     for (let i = 0; i < a.length; i++) {
         let b = a[i].getElementsByClassName('column')[0];
         let c = b.getElementsByClassName('text')[0];
-  
+
         let TextValue = c.textContent || c.innerText;
         if (TextValue.toUpperCase().indexOf(filter) > -1) {
             a[i].style.display = ''; // Show the element if it matches the filter
+            noMatches = false;
         } else {
             a[i].style.display = 'none'; // Hide the element if it doesn't match the filter
         }
     }
-  });
-  
-  
-  
+
+    // If no matches were found in the list, redirect to Google search
+    if (noMatches && filter.trim() !== '') {
+        const googleSearchURL = `https://www.google.com/search?q=${encodeURIComponent(filter)}`;
+        window.location.href = googleSearchURL;
+    }
+});
